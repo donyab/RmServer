0abde8777264115fd7106e686b09374baa974976
donyab <t303t@ya.ru>
2018-08-10 00:21:56 +0300

    Configure base project and create Hello world

---
+++ src/main/java/ru/britishdesign/rm/server/controller/ReminderController.java
@@ -0,0 +1,21 @@
+package ru.britishdesign.rm.server.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/reminder")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getReminder(ModelMap model){
+        return "My app BD";
+
+    }
+
+}
