4db4fc04694647f268c3bfb65fc633f22154af7c
donyab <t303t@ya.ru>
2018-08-15 19:21:55 +0300

    Create JSON request1

--- src/main/java/ru/britishdesign/rm/server/ApplicationInitializer.java
+++ src/main/java/ru/britishdesign/rm/server/ApplicationInitializer.java
@@ -1,10 +1,10 @@
 package ru.britishdesign.rm.server;
 
+import ru.britishdesign.rm.server.config.WebConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import ru.britishdesign.rm.server.config.WebConfig;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -12,14 +12,14 @@ import javax.servlet.ServletRegistration;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
 
-    private final static  String DISPATHER = "dispather";
+    private final static  String DISPATCHER = "dispatcher";
 
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(WebConfig.class);
         servletContext.addListener(new ContextLoaderListener(ctx));
 
-        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATHER, new DispatcherServlet(ctx));
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));
         servlet.addMapping("/");
         servlet.setLoadOnStartup(1);
     }
--- src/main/java/ru/britishdesign/rm/server/controller/ReminderController.java
+++ src/main/java/ru/britishdesign/rm/server/controller/ReminderController.java
@@ -1,21 +1,32 @@
 package ru.britishdesign.rm.server.controller;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.britishdesign.rm.server.entity.Remind;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model){
-        return "My app BD";
+    public Remind getReminder(){
+        return createMockRemind();
 
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My first reminde");
+
+        return remind;
     }
 
 }
